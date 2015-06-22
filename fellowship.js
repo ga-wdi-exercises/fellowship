0dda6aca032b8165c54abb6793c2a330c4a52af5
Jesse Shawl <jesse@jshawl.com>
2015-06-22 09:06:14 -0400

    prepare fellowship hw for DOM and objects lesson

--- fellowship.js
+++ fellowship.js
@@ -1,36 +1,48 @@
 // Dramatis Personae
-
-var hobbits = [
-  "Frodo Baggins",
-  "Samwise 'Sam' Gamgee",
-  "Meriadoc \"Merry\" Brandybuck",
-  "Peregrin 'Pippin' Took"
-];
-
-var buddies = [
-  "Gandalf the Grey",
-  "Legolas",
-  "Gimli",
-  "Strider",
-  "Boromir"
-];
-
-var lands = ["The Shire", "Rivendell", "Mordor"];
-
-function makeMiddleEarth(lands) {
-}
-
-function makeHobbits(hobbits) {
-}
-
-function keepItSecretKeepItSafe() {
-}
-
-function makeBuddies(buddies) {
-}
-
-function beautifulStranger() {
-}
-
-function forgeTheFellowShip() {
+var fellowship = {
+  hobbits: [
+    "Frodo Baggins",
+    "Samwise 'Sam' Gamgee",
+    "Meriadoc \"Merry\" Brandybuck",
+    "Peregrin 'Pippin' Took"
+  ],
+  buddies: [
+    "Gandalf the Grey",
+    "Legolas",
+    "Gimli",
+    "Strider",
+    "Boromir"
+  ],
+  lands: [
+    "The Shire", "Rivendell", "Mordor"
+  ],
+  makeMiddleEarth: function(lands) {
+    console.log(lands)
+    // create a section tag with an id of middle-earth
+    // add each land as an article tag
+    // inside each article tag include an h1 with the name of the land
+    // append middle-earth to your document body
+  },
+  makeHobbits: function(hobbits) {
+    // display an unordered list of hobbits in the shire
+    // give each hobbit a class of hobbit
+  },
+  keepItSecretKeepItSafe: function() {
+    // create a div with an id of 'the-ring'
+    // add the ring as a child of Frodo
+  },
+  makeBuddies: function(buddies) {
+    // create an aside tag
+    // display an unordered list of buddies in the aside
+    // insert your aside before rivendell
+  },
+  beautifulStranger: function() {
+    // change the buddy 'Strider' textnode to "Aragorn"
+  },
+  forgeTheFellowShip: function() {
+    // move the hobbits and the buddies to Rivendell
+    // create a new div called 'the-fellowship'
+    // add each hobbit and buddy one at a time to 'the-fellowship'
+    // after each character is added make an alert that they have joined your party
+  }
 }
