e47ab83ab9375fb59f46907c513aecc47cde110e
Alexandra <akscreven@gmail.com>
2015-10-17 20:00:44 -0400

    first problem done

--- fellowship.js
+++ fellowship.js
@@ -1,62 +1,64 @@
 // Dramatis Personae
-
-var  hobbits: [
+var fellowship = {
+ hobbits: [
     "Frodo Baggins",
     "Samwise 'Sam' Gamgee",
     "Meriadoc \"Merry\" Brandybuck",
     "Peregrin 'Pippin' Took"
-  ]
-var  buddies: [
+  ],
+buddies: [
     "Gandalf the Grey",
     "Legolas",
     "Gimli",
     "Strider",
     "Boromir"
-  ]
-var lands: [
+  ],
+lands: [
     "The Shire", "Rivendell", "Mordor"
-  ]
-function  makeMiddleEarth(lands){
+  ],
+makeMiddleEarth: function(lands){
     console.log(lands);
     var mid=document.createElement('section');
     mid.setAttribute('class','test');
     mid.setAttribute('id','middle-earth');
-    for (var i = 0; i <= lands.length; i+=1){
+    for (var i = 0; i < lands.length; i+=1){
       var art = document.createElement('article');
       var land = document.createElement('h1');
       land.innerHTML=lands[i];
-      art.appendTo(land);
-      mid.appendTo(art);
+      art.appendChild(land);
+      mid.appendChild(art);
     }
-    body.appendTo(mid);
+    document.body.appendChild(mid);
   }
-
-
-    // create a section tag with an id of middle-earth
-    // add each land as an article tag
-    // inside each article tag include an h1 with the name of the land
-    // append middle-earth to your document body
-
-  makeHobbits: function(hobbits) {
-    // display an unordered list of hobbits in the shire
-    // give each hobbit a class of hobbit
-  },
-  keepItSecretKeepItSafe: function() {
-    // create a div with an id of 'the-ring'
-    // add the ring as a child of Frodo
-  },
-  makeBuddies: function(buddies) {
-    // create an aside tag
-    // display an unordered list of buddies in the aside
-    // insert your aside before rivendell
-  },
-  beautifulStranger: function() {
-    // change the buddy 'Strider' textnode to "Aragorn"
-  },
-  forgeTheFellowShip: function() {
-    // move the hobbits and the buddies to Rivendell
-    // create a new div called 'the-fellowship'
-    // add each hobbit and buddy one at a time to 'the-fellowship'
-    // after each character is added make an alert that they have joined your party
-  }
-}
+};
+//
+//
+//
+//     // create a section tag with an id of middle-earth
+//     // add each land as an article tag
+//     // inside each article tag include an h1 with the name of the land
+//     // append middle-earth to your document body
+//
+//   makeHobbits: function(hobbits) {
+//     // display an unordered list of hobbits in the shire
+//     // give each hobbit a class of hobbit
+//   },
+//   keepItSecretKeepItSafe: function() {
+//     // create a div with an id of 'the-ring'
+//     // add the ring as a child of Frodo
+//   },
+//   makeBuddies: function(buddies) {
+//     // create an aside tag
+//     // display an unordered list of buddies in the aside
+//     // insert your aside before rivendell
+//   },
+//   beautifulStranger: function() {
+//     // change the buddy 'Strider' textnode to "Aragorn"
+//   },
+//   forgeTheFellowShip: function() {
+//     // move the hobbits and the buddies to Rivendell
+//     // create a new div called 'the-fellowship'
+//     // add each hobbit and buddy one at a time to 'the-fellowship'
+//     // after each character is added make an alert that they have joined your party
+//   }
+// }
