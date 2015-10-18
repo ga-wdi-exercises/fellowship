381f111f39b70caaa4794e3408aaffeeafcd695b
Alexandra <akscreven@gmail.com>
2015-10-18 19:22:55 -0400

    problem 2 complete

--- fellowship.js
+++ fellowship.js
@@ -17,29 +17,47 @@ lands: [
     "The Shire", "Rivendell", "Mordor"
   ],
 makeMiddleEarth: function(lands){
-    console.log(lands);
-    var mid=document.createElement('section');
-    mid.setAttribute('class','test');
-    mid.setAttribute('id','middle-earth');
-    for (var i = 0; i < lands.length; i+=1){
-      var art = document.createElement('article');
-      var land = document.createElement('h1');
-      land.innerHTML=lands[i];
-      art.appendChild(land);
-      mid.appendChild(art);
-    }
-    document.body.appendChild(mid);
+  console.log(lands);
+  var mid=document.createElement('section');
+  mid.setAttribute('class','test');
+  mid.setAttribute('id','middle-earth');
+  for (var i = 0; i < lands.length; i+=1){
+    var art = document.createElement('article');
+    var land = document.createElement('h1');
+    art.setAttribute('id',lands[i]);
+    land.setAttribute('class',lands[i]);
+    land.innerHTML=lands[i];
+    art.appendChild(land);
+    mid.appendChild(art);
   }
-};
-//
-//
-//
+  document.body.appendChild(mid);
+},
+
 //     // create a section tag with an id of middle-earth
 //     // add each land as an article tag
 //     // inside each article tag include an h1 with the name of the land
 //     // append middle-earth to your document body
 //
-//   makeHobbits: function(hobbits) {
+  makeHobbits: function(hobbits){
+    console.log(hobbits);
+    var liveInShire = document.createElement('ul');
+    for (var i = 0; i < hobbits.length; i++){
+      var newHobbit = document.createElement('li');
+      newHobbit.setAttribute('class','hobbit');
+      liveInShire.appendChild(newHobbit);
+      var resident = document.createTextNode(hobbits[i]);
+      newHobbit.appendChild(resident);
+    }
+    var locateShire = document.getElementById('The Shire');
+    locateShire.appendChild(liveInShire);
+  }
+};
+fellowship.makeMiddleEarth(fellowship.lands);
+fellowship.makeHobbits(fellowship.hobbits);
+
+  //   }
+
+  // },
 //     // display an unordered list of hobbits in the shire
 //     // give each hobbit a class of hobbit
 //   },
