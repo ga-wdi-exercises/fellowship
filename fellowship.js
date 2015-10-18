1f71c5c4251aac23139a37589a38cd5544977ea0
alea q <aleaq@aleas-AppleSmoothie.local>
2015-10-18 18:18:23 -0400

    final document

--- fellowship.js
+++ fellowship.js
@@ -1,3 +1,19 @@
+
+//exmaple to follow:
+// var btn = document.createElement("BUTTON");        // Create a <button> element
+// var t = document.createTextNode("CLICK ME");       // Create a text node
+// btn.appendChild(t);                                // Append the text to <button>
+// document.body.appendChild(btn);
+// var el = document.createElement("p");
+// el.appendChild(document.createTextNode("Test"));
+//
+// var tmp = document.createElement("div");
+// tmp.appendChild(el);
+// console.log(tmp.innerHTML);
+
+
+
+
 // Dramatis Personae
 var fellowship = {
   hobbits: [
@@ -16,29 +32,35 @@ var fellowship = {
   lands: [
     "The Shire", "Rivendell", "Mordor"
   ],
+
+
   makeMiddleEarth: function(lands) {
-    console.log(lands)
     // create a section tag with an id of middle-earth
-        var middleEarth = document.createElement("section");
-    // add each land as an article tag
-    var theShire = document.createElement("article")
-    var Rivendell = document.createElement("article")
-    var Mordor = document.createElement("article")
-    // inside each article tag include an h1 with the name of the land
-    // append middle-earth to your document body
-    var h1 = document.createElement("h1")
-      theShire.appendChild(h1);
-        document.getElementsByTagName("h1").innerHTML =fellowship.lands[0];
-      Rivendell.appendChild(h1);
-        document.getElementsByTagName("h1").innerHTML =fellowship.lands[1];
-      Mordor.appendChild(h1);
-        document.getElementsByTagName("h1").innerHTML =fellowship.lands[2];
-  },
+    var middleEarth = document.createElement("section");
+    middleEarth.id = "middle-earth";
+    for (var i=0; i<fellowship.lands.length; i++)
+    {
+      var articleTag = document.createElement("article");
+      var h1Tag = document.createElement("h1");
+      h1Tag.textContent = fellowship.lands[i]; //gets each name and adds to h1
+      articleTag.appendChild(h1tag); //appends h1 to acticle
+      middleEarth.appendChild(articleTag); //aapends article to section
+
+    }
+  document.body.appendChild(middleEarth)
+
+      },
+
   makeHobbits: function(hobbits) {
     // display an unordered list of hobbits in the shire
-    fellowship.hobbits;
+    var hobbitsUL = fellowship.hobbits
+    console.log(hobbitsUL)
     // give each hobbit a class of hobbit
-  },
+  var addClass = fellowship.hobbits.length; //gets total # of hobbits in array
+  for (i =0; i< addClass.length; i++) {
+    "class" += ' ' + hobbitsUL[i];
+  }
+},
   keepItSecretKeepItSafe: function() {
     // create a div with an id of 'the-ring'
     // add the ring as a child of Frodo
@@ -56,5 +78,7 @@ var fellowship = {
     // create a new div called 'the-fellowship'
     // add each hobbit and buddy one at a time to 'the-fellowship'
     // after each character is added make an alert that they have joined your party
-  }
+   }
+
 }
+fellowship.makeMiddleEarth(fellowship.lands);
