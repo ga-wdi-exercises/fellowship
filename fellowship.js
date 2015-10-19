fecd22e7f96c31051784a3629fb140da3326802d
Cassidy Harpster <cassidy.j.harpster@gmail.com>
2015-10-19 08:55:29 -0400

    second try

--- fellowship.js
+++ fellowship.js
@@ -20,16 +20,16 @@ var fellowship = {
     console.log(lands)
     // create a section tag with an id of middle-earth
     var newSection = document.createElement('section');
-    newSection.setAttribute('id', 'middle-earth');
+    newSection.setAttribute('id', 'middleearth');
     // add each land as an article tag
     for(var i = 0; i < fellowship.lands.length; i++) {
       var newLands = document.createElement('article');
-      newLands.setAttribute('id', fellowship.lands[i]);
+      newHeader.innerHTML = fellowship.lands[i];
     // inside each article tag include an h1 with the name of the land
       var landNames = document.createElement('h1');
       newLands.textContent = fellowship.lands[i];
     // append middle-earth to your document body
-    document.body.appendChild('middle-earth');
+    document.body.appendChild('middleearth');
   },
   makeHobbits: function(hobbits) {
     // display an unordered list of hobbits in the shire
@@ -38,18 +38,26 @@ var fellowship = {
     for(var i = 0; i < fellowship.hobbits.length; i++) {
       hobbitList.textContent('ul', fellowship.hobbits[i]);
     // give each hobbit a class of hobbit
+
   },
   keepItSecretKeepItSafe: function() {
+    var ring = document.createElement('div');
+    ring.setAttribute('id', 'the-ring');
     // create a div with an id of 'the-ring'
+    document.body.appendChild('Frodo Baggins');
     // add the ring as a child of Frodo
   },
   makeBuddies: function(buddies) {
+    var asidetag = document.createElement('asidetag');
+    asidetag.setAttribute('id', 'listofbuddies');
     // create an aside tag
     // display an unordered list of buddies in the aside
+    var x = document.getElementById("buddies").textContent;
     // insert your aside before rivendell
   },
   beautifulStranger: function() {
     // change the buddy 'Strider' textnode to "Aragorn"
+    fellowship.buddies.textContent('Strider', 'Aragorn');
   },
   forgeTheFellowShip: function() {
     // move the hobbits and the buddies to Rivendell
