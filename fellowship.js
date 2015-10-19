c2f5963093a0810584858e1b8fdcc9f6a7d6cfc3
Daniel Alexander <daniel.elliot.alexander@gmail.com>
2015-10-18 22:56:01 -0400

    added java exercise

--- fellowship.js
+++ fellowship.js
@@ -16,18 +16,43 @@ var fellowship = {
   lands: [
     "The Shire", "Rivendell", "Mordor"
   ],
-  makeMiddleEarth: function(lands) {
-    console.log(lands)
+
+  function makeMiddleEarth(lands) {
+    var middleEarth = document.createElement('section');
+    middleEarth.id = "middle-earth";
+    for (i=0; i <lands.length; i++) {
+      var makeArticle = document.createElement('article');
+      var makeH1 = document.createElement('H1');
+      makeArticle.id = lands[i];
+      makeH1.textContent = lands[i];
+      makeArticle.appendChild(makeH1);
+      middleEarth.appendChild(makeArticle);
+    }
+    document.body.appendChild(middleEarth);
+  }
+  makeMiddleEarth(lands);
     // create a section tag with an id of middle-earth
     // add each land as an article tag
     // inside each article tag include an h1 with the name of the land
     // append middle-earth to your document body
   },
-  makeHobbits: function(hobbits) {
+  function makeHobbits(hobbits) {
+    var hobList = document.createElement('ul');
+    makeMiddleEarth.appendChild(hobList);
+    for (i=0; i <hobbits.length; i++){
+        var makeLi = document.createElement('li');
+        makeLi.id = "hobbit";
+        hobList.appendChild(makeLi);
+        makeLi.textContent = hobbits[i]
+    }
+  makeHobbits(hobbits);
     // display an unordered list of hobbits in the shire
     // give each hobbit a class of hobbit
   },
   keepItSecretKeepItSafe: function() {
+    var makeDiv = document.createElement('div');
+    makeDiv.id = 'the-ring';
+
     // create a div with an id of 'the-ring'
     // add the ring as a child of Frodo
   },
@@ -44,5 +69,6 @@ var fellowship = {
     // create a new div called 'the-fellowship'
     // add each hobbit and buddy one at a time to 'the-fellowship'
     // after each character is added make an alert that they have joined your party
+
   }
 }
