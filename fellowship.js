68e1f13ac6430def4d8de9aa2fb5dfc3c8e33a38
ckib16 <ckib16@gmail.com>
2015-10-18 23:22:38 -0400

    Horrible but pushing anyway

--- fellowship.js
+++ fellowship.js
@@ -14,18 +14,34 @@ var fellowship = {
     "Boromir"
   ],
   lands: [
-    "The Shire", "Rivendell", "Mordor"
+    "The Shire",
+    "Rivendell",
+    "Mordor"
   ],
   makeMiddleEarth: function(lands) {
-    console.log(lands)
+    console.log(lands);
     // create a section tag with an id of middle-earth
+    var sectionElement = document.createElement("section");
+    sectionElement.id = "middle-earth";
+    document.body.appendChild(sectionElement);
     // add each land as an article tag
     // inside each article tag include an h1 with the name of the land
+
+    for (var i = 0; i < fellowship.lands.length; i++) {
+      var articleElement = document.createElement("article");
+      var h1Element = document.createElement('H1');
+      h1Element.appendChild(articleElement);
+      h1Element.textContent = lands[i];
+
     // append middle-earth to your document body
+    document.body.appendChild(middle-earth);
+    }
   },
   makeHobbits: function(hobbits) {
     // display an unordered list of hobbits in the shire
+    var listofHobbits = document.createElement(ul);
     // give each hobbit a class of hobbit
+
   },
   keepItSecretKeepItSafe: function() {
     // create a div with an id of 'the-ring'
@@ -45,4 +61,4 @@ var fellowship = {
     // add each hobbit and buddy one at a time to 'the-fellowship'
     // after each character is added make an alert that they have joined your party
   }
-}
+};
