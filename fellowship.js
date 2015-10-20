c54f8724ac807b8a93add29aa131406afe97a496
jen pengelly <jgpengelly@gmail.com>
2015-10-20 10:18:01 -0400

    fellowship.js: updated psuedo code based on review. index: removed middle-earth section

--- fellowship.js
+++ fellowship.js
@@ -1,50 +1,48 @@
 // Dramatis Personae
 
-//something is wrong with this array. commenting out solves issue with middleEarth function.
+var fellowship = {
+  hobbits: [
+    "Frodo Baggins",
+    "Samwise 'Sam' Gamgee",
+    "Meriadoc \"Merry\" Brandybuck",
+    "Peregrin 'Pippin' Took"
+  ],
+  buddies: [
+    "Gandalf the Grey",
+    "Legolas",
+    "Gimli",
+    "Strider",
+    "Boromir"
+  ],
+  lands: [
+    "The Shire", "Rivendell", "Mordor"
+  ],
 
-// var fellowship = {
-//   hobbits: [
-//     "Frodo Baggins",
-//     "Samwise 'Sam' Gamgee",
-//     "Meriadoc \"Merry\" Brandybuck",
-//     "Peregrin 'Pippin' Took"
-//   ],
-//   buddies: [
-//     "Gandalf the Grey",
-//     "Legolas",
-//     "Gimli",
-//     "Strider",
-//     "Boromir"
-//   ],
-//   lands: [
-//     "The Shire", "Rivendell", "Mordor"
-//   ],
-
-// need to revise function syntax for all the pre-written functions
-  function makeMiddleEarth(lands) {
+  makeMiddleEarth: function(lands) {
+    console.log(lands)
     // create a section tag with an id of middle-earth
       // this code creates the section tag and the gives it the "middle-earth" id.
     var middleEarth = document.createElement("section");
     middleEarth.id = "middle-earth";
     // add each land as an article tag
-    // this for loop lets me iterate through the different entries in the land array. Needs to come before my variables declaring the article tag and h1 tag (scope issue??)
-    for (var i = 0; i < lands.length; i++) {
-      var articleTag = document.createElement("article");
-    // inside each article tag include an h1 with the name of the land
-      var h1Tag = document.createElement("h1");
-      h1Tag.innerHTML = lands[i];
-      // this code creates the h1 tag within the article tag
-      articleTag.appendChild(h1Tag);
-      // this code creates the article tag within the middle earth variable.
-      middleEarth.appendChild(articleTag);
-    }
+      // loop through each land
+      for (var i = 0; i < lands.length; i++) {
+      // create an article tag for each land
+        var articleTag = document.createElement("article");
+      // Add h1 inside each article
+        var h1Tag = document.createElement("h1");
+      // change innerHTML of each h1 to be land name
+        h1Tag.innerHTML = lands[i];
+      // appends h1 to each article
+        articleTag.appendChild(h1Tag);
+     // append the article to the section with id of middle-earth
+        middleEarth.appendChild(articleTag);
+      }
     // append middle-earth to your document body
-    document.body.appendChild(middleEarth);
-  }
-  makeMiddleEarth(lands);
-console.log(lands);
+      document.body.appendChild(middleEarth);
+    },
 
-   function makeHobbits(hobbits) {
+  makeHobbits: function(hobbits) {
      // display an unordered list of hobbits in the shire
      // give each hobbit a class of hobbit
     var listHobbits = document.createElement('ul');
@@ -57,8 +55,6 @@ console.log(lands);
     var shire = document.getElementbyTagName("article")[0];
     shire.appendChild(listHobbits)
    }
-   makeHobbits(hobbits);
-
 
 //   keepItSecretKeepItSafe: function() {
 //     // create a div with an id of 'the-ring'
@@ -79,3 +75,5 @@ console.log(lands);
 //     // after each character is added make an alert that they have joined your party
 //   }
 }
+
+fellowship.makeMiddleEarth(fellowship.lands);
